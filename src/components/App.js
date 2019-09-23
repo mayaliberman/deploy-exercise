2e9cd8f5e78453091553185283d65641636dfe4a
Maya Liberman <mayaliberman1@gmail.com>
2019-09-23 16:17:37 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 //App components
 import Header from './Header';
 import Home from './Home';
