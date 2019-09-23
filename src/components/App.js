a2ec6597474c30807b91a565a1d2378f2bdf1ee8
Maya Liberman <mayaliberman1@gmail.com>
2019-09-23 14:17:29 +0300

    deployment1

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//App components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Courses from './Courses';
+import Teachers from './Teachers';
+import NotFound from './NotFound';
+import Featured from './Featured';
+const App = () => (
+  <BrowserRouter>
+    <div className='container'>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/about" render={() => <About title='About'/>}></Route>
+      <Route exact path="/teachers" component={Teachers}></Route>
+      <Route path="/teachers/:topic/:name" component={Featured}></Route>
+      <Route path="/courses" component={Courses}></Route>
+      <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
