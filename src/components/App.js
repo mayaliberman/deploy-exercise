7ef287c7c3ed4d506d2e4142f3456ae5019017db
Maya Liberman <mayaliberman1@gmail.com>
2019-09-23 16:15:47 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,16 @@ import Teachers from './Teachers';
 import NotFound from './NotFound';
 import Featured from './Featured';
 const App = () => (
-  <HashRouter  >
+  <HashRouter>
     <div className='container'>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/about" render={() => <About title='About'/>}></Route>
-      <Route exact path="/teachers" component={Teachers}></Route>
-      <Route path="/teachers/:topic/:name" component={Featured}></Route>
-      <Route path="/courses" component={Courses}></Route>
-      <Route component={NotFound} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route path='/about' render={() => <About title='About' />}></Route>
+        <Route exact path='/teachers' component={Teachers}></Route>
+        <Route path='/teachers/:topic/:name' component={Featured}></Route>
+        <Route path='/courses' component={Courses}></Route>
+        <Route component={NotFound} />
       </Switch>
     </div>
   </HashRouter>
